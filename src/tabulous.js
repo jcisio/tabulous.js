3d2e0c92f3ec90bc082e68739abfd35603bcec4b
Hai-Nam Nguyen <hainam@jcisio.com>
2013-10-28 23:07:29 +0100

    Reformat code

--- src/tabulous.js
+++ src/tabulous.js
@@ -8,13 +8,13 @@
     links.find('li:last-child').after('<span style="clear:both;display:block"/>');
     tabs.find('>div').addClass('make_transist');
     show(links, tabs, 1);
-    links.on('click', 'a', function() {
+    links.on('click', 'a', function () {
       var n = links.find('a').index(this) + 1;
       show(links, tabs, n);
     });
   };
 
-  var show = function(links, tabs, n) {
+  var show = function (links, tabs, n) {
     links.find('li:not(:nth-child(' + n + ')) a').removeClass('active');
     links.find('li:nth-child(' + n + ') a').addClass('active');
     tabs.find('>div:not(:nth-child(' + n + '))').removeClass('showleft').addClass('hideleft');
@@ -22,7 +22,7 @@
     tabs.css('height', tabs.find('>div:nth-child(' + n + ')').height() + 'px');
   };
 
-  $.fn.tabulous = function() {
+  $.fn.tabulous = function () {
     return this.each(function () {
       init(this);
     });
